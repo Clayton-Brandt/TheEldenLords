f34b024f41e41e0656bf9cbcbad541d36eab062d
wdryan1 <ryanw1@etsu.edu>
2026-04-22 14:17:31 -0400

    changed bosses and completion.py
    should say updated

--- API/API/bosses.py
+++ API/API/bosses.py
@@ -1,66 +1,57 @@
 @router.get("/")
-def get_posts(title: str | None = None):
+def get_bosses(name: str | None = None):
     conn = get_db()
     cur = conn.cursor()
 
-    if title:
-        cur.execute("SELECT * FROM posts WHERE title LIKE ?", (f"%{title}%",))
+    if name:
+        cur.execute("SELECT * FROM bosses WHERE name LIKE ?", (f"%{name}%",))
     else:
-        cur.execute("SELECT * FROM posts")
+        cur.execute("SELECT * FROM bosses")
 
     rows = cur.fetchall()
     return [dict(row) for row in rows]
 
-@router.get("/{post_id}")
-def get_post(post_id: int):
+@router.get("/{boss_id}")
+def get_boss(boss_id: int):
     conn = get_db()
     cur = conn.cursor()
-    cur.execute("SELECT * FROM posts WHERE id=?", (post_id,))
+    cur.execute("SELECT * FROM bosses WHERE id=?", (boss_id,))
     row = cur.fetchone()
 
     if not row:
-        raise HTTPException(404, "Post not found")
+        raise HTTPException(404, "Boss not found")
 
     return dict(row)
 
 
 @router.post("/")
-def create_post(post: Post):
+def create_boss(boss: Boss):
     conn = get_db()
     cur = conn.cursor()
     cur.execute(
-        "INSERT INTO posts (user_id, title, post_text, likes) VALUES (?, ?, ?, ?)",
-        (post.user_id, post.title, post.post_text, post.likes)
+        "INSERT INTO bosses (name, location, lore, stats, prerequisites, help, rewards) VALUES (?, ?, ?, ?, ?, ?, ?)",
+        (boss.name, boss.location, boss.lore, boss.stats, boss.prerequisites, boss.help, boss.rewards)
     )
     conn.commit()
-    return "Post created"
+    return "Boss created"
 
-@router.put("/{post_id}")
-def update_post(post_id: int, post: PostUpdate):
+@router.put("/{boss_id}")
+def update_boss(boss_id: int, boss: BossUpdate):
     conn = get_db()
     cur = conn.cursor()
     cur.execute(
-        "UPDATE posts SET user_id=?, title=?, post_text=?, likes=? WHERE id=?",
-        (post.user_id, post.title, post.post_text, post.likes, post_id)
-    )
+        "UPDATE bosses SET name=?, location=?, lore=?, stats=?, prerequisites=?, help=?, rewards=? WHERE id=?",
+        (boss.name, boss.location, boss.lore, boss.strats, boss.prerequisites, boss.help, boss.rewards, boss_id))
     conn.commit()
-    return "Post updated"
+    return "Boss updated"
 
-@router.patch("/{post_id}/title")
-def patch_title(post_id: int, title: str):
+@router.delete("/{boss_id}")
+def delete_boss(boss_id: int):
     conn = get_db()
     cur = conn.cursor()
-    cur.execute("UPDATE posts SET title=? WHERE id=?", (title, post_id))
+    cur.execute("DELETE FROM bosses WHERE id=?", (boss_id,))
     conn.commit()
-    return "Title updated"
-
-@router.delete("/{post_id}")
-def delete_post(post_id: int):
-    conn = get_db()
-    cur = conn.cursor()
-    cur.execute("DELETE FROM posts WHERE id=?", (post_id,))
-    conn.commit()
-    return "Post deleted"
+    return "boss deleted"
 
 
 
--- API/API/completion.py
+++ API/API/completion.py
@@ -1,16 +1,16 @@
-@router.get("/user/{user_id}")
-def get_posts_by_user(user_id: int):
+@router.get("/{user_id}")
+def get_completion_by_user(user_id: int):
     conn = get_db()
     cur = conn.cursor()
-    cur.execute("SELECT * FROM posts WHERE user_id=?", (user_id,))
+    cur.execute("SELECT * FROM completion WHERE user_id=?", (user_id,))
     rows = cur.fetchall()
     return [dict(row) for row in rows]
 
-@router.get("/{post_id}")
-def get_post(post_id: int):
+@router.get("/{user_id}/{boss_id}")
+def get_completion_entry(user_id: int, boss_id: int):
     conn = get_db()
     cur = conn.cursor()
-    cur.execute("SELECT * FROM posts WHERE id=?", (post_id,))
+    cur.execute("SELECT * FROM completion WHERE user_id=? AND boss_id=?" (user_id, boss_id))
     row = cur.fetchone()
 
     if not row:
@@ -19,14 +19,14 @@ def get_post(post_id: int):
     return dict(row)
 
 @router.post("/")
-def create_post(post: Post):
+def create_completion(entry: Completion):
     conn = get_db()
     cur = conn.cursor()
     cur.execute(
-        "INSERT INTO posts (user_id, title, post_text, likes) VALUES (?, ?, ?, ?)",
-        (post.user_id, post.title, post.post_text, post.likes)
+        "INSERT INTO completion (user_id, boss_id, completed) VALUES (?, ?, ?)",
+        (entry.user_id, entry.boss_id, entry.completed,)
     )
     conn.commit()
-    return "Post created"
+    return "Completion entry created"
 
 
