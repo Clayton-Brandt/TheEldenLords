79aac09d0cd344b649e7233a18b9de83424ef663
wdryan1 <ryanw1@etsu.edu>
2026-04-30 14:11:04 -0400

    updated
    fixed boss stats and fixed linking the database to api

--- API/stats.py
+++ API/stats.py
@@ -9,10 +9,10 @@ def get_boss_stats(boss_id: int):
     conn = get_db()
     cur = conn.cursor()
 
-    cur.execute("SELECT stat_name, stat_value FROM boss_stats WHERE boss_id=?", (boss_id,))
-    rows = cur.fetchall()
+    cur.execute("SELECT * FROM boss_stats WHERE boss_id=?", (boss_id,))
+    row = cur.fetchone()
 
-    if not rows:
+    if not row:
         raise HTTPException(404, "No stats found for this boss")
 
-    return [dict(row) for row in rows]
+    return dict(row)
